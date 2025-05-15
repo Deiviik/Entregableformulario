5a60970b7908322297a887020f3a0ece8f39e9d6
David Collazos <juandavidibarracollazos@gmail.com>
2025-05-14 21:46:50 -0500

    formulario funcional con el correo electronico

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ nextBtn.addEventListener('click', () => {
 // Mostrar el primero al cargar
 mostrarSlide(index);
 
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que se recargue la página
+
+  const nombre = document.getElementById("nombre").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mensaje = document.getElementById("mensaje").value.trim();
+
+  if (nombre && email && mensaje) {
+    document.getElementById("formMsg").textContent = "¡Gracias por contactarnos!";
+    this.reset();
+  } else {
+    document.getElementById("formMsg").textContent = "Por favor, completa todos los campos.";
+    document.getElementById("formMsg").style.color = "red";
+  }
+});
